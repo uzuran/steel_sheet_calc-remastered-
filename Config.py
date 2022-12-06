a4e098253039ca7dc9d91d7d03ce341176dcce5e
uzuran <archloverlinux@gmail.com>
2022-12-07 00:02:45 +0100

    Create config #19 - Create Config.py for separate configure UI

--- Config.py
+++ Config.py
@@ -1,13 +1,30 @@
+import configparser
+
+config = configparser.ConfigParser()
+
+config["DEFAULT"] = {"title": "Config.py",
+                     "compression": "yes",
+                     "compression_level": "9"}
+
+config["Config.py"] = {}
+configuration = config["Config.py"]
+
 # Font for all strings.
-font = "Calibri, 12"
+configuration["Font"] = "Calibri, 12"
 
 # Config for buttons at the beginning of program
-main_button_width = "30"
-main_button_height = "2"
+configuration["Main_button_width"] = "30"
+configuration["Main_button_height"] = "30"
 
 # Config for registration user_button, and login
-lr_button_width = "17"
-lr_button_height = "2"
+configuration["lr_button_width"] = "17"
+configuration["lr_button_height"] = "2"
+
+
+# Config for registration user_button, and login
+configuration["lr_button_width"] = "17"
+configuration["lr_button_height"] = "2"
+configuration["lr_button_height"] = "2"
 
 # Main config for main labels.
 main_background = "#d1dffa"
